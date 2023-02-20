f322ce1f218642705369750db9c94253c7a4f6b4
Gabor Csepregi <gabor.csepregi@accenture.com>
2023-02-20 20:57:05 +0100

    shopping cart

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import ProductDetailsScreen from "./src/screens/ProductDetailsScreen";
 import ProductScreen from "./src/screens/ProductScreen";
+import ShoppingCart from "./src/screens/ShoppingCart";
 
 export default function App() {
   return (
     <View style={styles.container}>
       {/* <ProductScreen /> */}
-      <ProductDetailsScreen />
+      {/* <ProductDetailsScreen /> */}
+      <ShoppingCart />
       <StatusBar style="auto" />
     </View>
   );
